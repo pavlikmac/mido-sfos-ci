359899bba30140f4a2b0e6c3b4464209706707b2
gmicroul <121873438+gmicroul@users.noreply.github.com>
2024-11-21 22:44:31 +0800

    Update run-mic.sh

--- run-mic.sh
+++ run-mic.sh
@@ -7,6 +7,8 @@ export PORT_ARCH="armv7hl"
 export EXTRA_NAME=-devel-`date +%Y%m%d`
 export RELEASE="5.0.0.29"
 
+sudo mkdir -p /proc/sys/fs/binfmt_misc/
+sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
 sudo mic create fs --arch=$PORT_ARCH \
 --tokenmap=ARCH:$PORT_ARCH,RELEASE:$RELEASE,EXTRA_NAME:$EXTRA_NAME \
 --record-pkgs=name,url \
