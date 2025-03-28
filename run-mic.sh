86251c56771524dc3ab1f84914c95dd150a0697d
pavlikmac <102363591+pavlikmac@users.noreply.github.com>
2025-03-28 18:05:35 +0800

    Update run-mic.sh

--- run-mic.sh
+++ run-mic.sh
@@ -5,7 +5,7 @@ export VENDOR="xiaomi"
 export DEVICE="mido"
 export PORT_ARCH="armv7hl"
 export EXTRA_NAME=-devel
-export RELEASE="5.0.0.61"
+export RELEASE="5.0.0.62"
 
 sudo mkdir -p /proc/sys/fs/binfmt_misc/
 sudo mount binfmt_misc -t binfmt_misc /proc/sys/fs/binfmt_misc
